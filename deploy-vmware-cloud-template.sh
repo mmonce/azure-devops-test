187f219eb0aa0b05ac1e331118a7fc485dd591e3
mmonce <mmonce@vmware.com>
2020-08-31 15:29:06 -0500

    Deploy Simple Cloud Template

---
+++ deploy-vmware-cloud-template.sh
@@ -0,0 +1,45 @@
+vracloudurl='https://api.mgmt.cloud.vmware.com'
+apitoken='1lUWrQ0qY3MauPM6KAlXJkeXs5bZUsW2Ghw3zSQb7rAtDLv75SRal4Ft7MnvQfuI'
+blueprint_name='Ubuntu-API'
+api_version='2019-09-12'
+
+echo $vracloudurl
+
+
+access_token=`curl -X POST \
+ "$vracloudurl/iaas/api/login" \
+ -H 'Content-Type: application/json' \
+ -s \
+ -d '{
+  "refreshToken":"'"$apitoken"'"
+}' -k | jq -r .token`
+
+
+
+echo $token
+
+# curl -X GET \
+# "$vracloudurl/csp/gateway/am/api/loggedin/user/orgs" \
+# -H, -k "csp-auth-token: $token"
+
+curl -X GET $vracloudurl/blueprint/api/blueprints -H "Authorization: Bearer $access_token" -k | jq "."
+
+blueprint_id='d2ba6fe0-3d82-4d03-93ae-556c7ceb89b2'
+image_mapping='ubuntu'
+flavor_mapping='small'
+
+echo $blueprint_id
+
+ curl -X POST \
+ $vracloudurl/blueprint/api/blueprint-requests?apiVersion=$api_version \
+ -H "Authorization: Bearer $access_token" \
+ -H 'Content-Type: application/json' \
+ -d '{
+ "description": "Blueprint Request",
+ "blueprintId": "'"$blueprint_id"'",
+ "inputs": {
+ 		"count": 1,
+ 		"image":"'"$image_mapping"'",
+ 		"flavor":"'"$flavor_mapping"'"
+ }
+}' -k | jq "."
